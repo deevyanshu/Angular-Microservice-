3e549788e50df6052d62588d3fa2817ce81d29a7
deevyanshu <devyanshugarg2000@gmail.com>
2022-10-17 20:24:34 +0530

    Add files via upload

---
+++ FormGateway/src/main/java/com/deevyanshu/FormGatewayApplication.java
@@ -0,0 +1,28 @@
+package com.deevyanshu;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class FormGatewayApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FormGatewayApplication.class, args);
+	}
+
+	
+	
+	
+	
+
+}
